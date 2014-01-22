fb71c8b56c0792023e479a36bf4f311107230255
iMultiPlay <imultiplay123@gmail.com>
2014-01-21 22:57:19 -0500

    added forecast function

--- weather.py
+++ weather.py
@@ -6,12 +6,14 @@ from random import randint
 
 def weatherdata():
     url = 'http://weather.yahooapis.com/forecastrss?w=' + str(config['INFO']['WOEID']) + '&u=' + str(config['INFO']['UNITS'])
+    #print(url)
     root = returnroot(url)
     day0 = root[0][12][7].attrib #Today, magically pulled from xml file
     day1 = root[0][12][8].attrib
     day2 = root[0][12][9].attrib
+    day3 = root[0][12][10].attrib
     wind0 = root[0][8].attrib
-    hold = [wind0, day0, day1, day2]
+    hold = [wind0, day0, day1, day2, day3]
     return hold
 
 
@@ -59,7 +61,7 @@ def quotes():
     quotes = []
     for n in root.iter('description'):
         quotes.append(n.text)
-    return quotes[1]
+    return quotes[len(quotes)-1]
        
 def returnroot(url):
     try:
@@ -85,26 +87,48 @@ def currentx():
     hold = [currTemp, currCond, currWind, currWDir]
     return hold
 
-def drawmenu(message, weather, quote, current):
+def forecastx(data):
+    day = [data[2], data[3], data[4]]
+    
+    hold = []
+    for i in range(0, len(day)):
+        hold.append((str(day[i]['day']), str(day[i]['high']), str(day[i]['low']), str(day[i]['text'])))
+    return hold
+    
+def drawmenu(message, weather, quote, current, forecast):
     lnc = int(eval(config['INFO']['LINE'])) #How many characters you want the screen to be wide (Line Count)
     high = weather[1]
     low = weather[0]
     genline = lambda mes: print(' '*((lnc - len(mes) -1)//2) + mes + ' '*((lnc - len(mes) -1)//2)) #Centre text on screen based on lnc
     
     ## Regular lines
-    wind = str(current[2]) + ' km/H ' + str(current[3]) + ' | ' + str(current[1])
-    weatherline = "High: " + str(high) + u'°' + "C" + " " + "Low: " + str(low) + u'°' + "C"  + ' | ' + wind 
+    wind = str(current[2]) + ' km/H ' + str(current[3]) + ' | ' + str(current[1]).capitalize()
+    weatherline = "High: " + str(high) + u'°' + "C" + " " + "Low: " + str(low) + u'°' + "C"  + ' | ' + wind
     Bweather = textwrap.wrap(weatherline, lnc - 5)#Ensure that lines fit line character limit
     Bmessage = textwrap.wrap(message, lnc - 5)
-    Bquote = textwrap.wrap(quote, lnc - 5)
-    
+    #print(quote)
+    try:
+        Bquote = textwrap.wrap(quote, lnc - 5)
+    except:
+        Bquote = textwrap.wrap('Feed a man a fish, he will eat for a day. Teach a man to fish, and he eat for a lifetime. - Bible Proverb', lnc-5)
+        
     ## Current conditions
     temp = 'Current Temperature: ' + str(current[0]) + u'°' + "C"
     
-    
-    
     Btemp = textwrap.wrap(temp, lnc - 5)
     #Bwind = textwrap.wrap(wind, lnc - 5)
+
+    ### Forcast
+    Fdata = []
+    #print(forecast[2])
+    for i in range(0, len(forecast)):
+        Fdata.append(str(forecast[i][0]) + ' == ' + ' High: ' + str(forecast[i][1]) + u'°' + "C" + ' Low: ' + str(forecast[i][2]) + u'°' + "C " + str(forecast[i][3])) 
+     
+    BFdata0 = textwrap.wrap(Fdata[0], lnc - 5)
+    BFdata1 = textwrap.wrap(Fdata[1], lnc - 5)
+    BFdata2 = textwrap.wrap(Fdata[2], lnc - 5)
+
+    ## Draw screen
     
     print('='*lnc) # 63 characters long
     print('')
@@ -120,6 +144,15 @@ def drawmenu(message, weather, quote, current):
     for i in Bmessage:
         genline(i)
     print('')
+    genline('Forcast:')
+    #print('')
+    for i in BFdata0:
+        genline(i)
+    for i in BFdata1:
+        genline(i)
+    for i in BFdata2:
+        genline(i)
+        
     print('='*lnc)
     print('')
     genline('Quote:')
@@ -134,9 +167,10 @@ def main():
     data = weatherdata()
     #print(data)
     messagex, weather = message(data)
+    forecast = forecastx(data)
     quote = quotes()
     current = currentx()
-    drawmenu(messagex, weather, quote, current)
+    drawmenu(messagex, weather, quote, current, forecast)
 
 global config
 configx = configparser.ConfigParser()
