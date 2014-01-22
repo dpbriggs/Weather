e3b788ed5ea874b6fb07c613788d84771e938c9b
iMultiPlay <imultiplay123@gmail.com>
2014-01-21 19:06:20 -0500

    Added current conditions

--- weather.py
+++ weather.py
@@ -1,15 +1,12 @@
 import xml.etree.ElementTree as etree
 import urllib.request
 import textwrap
+import configparser
 from random import randint
 
 def weatherdata():
-    url = 'http://weather.yahooapis.com/forecastrss?w=6&u=c'
-    try:
-        xmltext = (urllib.request.urlretrieve(url))
-    except:
-        print('Internet connection or WOEID is not valid')
-    root = (etree.parse(xmltext[0])).getroot()
+    url = 'http://weather.yahooapis.com/forecastrss?w=' + str(config['INFO']['WOEID']) + '&u=' + str(config['INFO']['UNITS'])
+    root = returnroot(url)
     day0 = root[0][12][7].attrib #Today, magically pulled from xml file
     day1 = root[0][12][8].attrib
     day2 = root[0][12][9].attrib
@@ -50,7 +47,7 @@ def message(data): #Generate small message to summarize weather (mostly based on
     
     ## Check if there's a large difference in high/low
     if abs(high) - abs(low) > 10:
-        message.append(" You may also want to dress in layers, the tempurature changes a lot")
+        message.append("You may also want to dress in layers, the tempurature changes a lot")
     messStr = ''
     for i in message:
         messStr = messStr + i
@@ -58,40 +55,70 @@ def message(data): #Generate small message to summarize weather (mostly based on
     return str(messStr), list(weather)
 
 def quotes():
-    url = 'http://www.quotesdaddy.com/feed'
-    try:
-        xmltext = (urllib.request.urlretrieve(url))
-    except:
-        print('Internet connection is not valid')
-    root = (etree.parse(xmltext[0])).getroot()
+    root = returnroot('http://www.quotesdaddy.com/feed')
     quotes = []
     for n in root.iter('description'):
         quotes.append(n.text)
     return quotes[1]
        
-            
-        
+def returnroot(url):
+    try:
+        xmltext = (urllib.request.urlretrieve(url))
+    except:
+        print('Internet connection or WOEID is not valid')
+    root = (etree.parse(xmltext[0])).getroot()
+    return root
+         
+def currentx():
+    units = str(config['INFO']['UNITS'])
+    city = str(config['INFO']['CITY'])
+    country = str(config['INFO']['COUNTRY'])
+    if units == 'c':
+        url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&units=metric&mode=xml'
+    else:
+        url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&units=imperial&mode=xml'
+    root = returnroot(url)
+    currWind = round(float(root[4][0].attrib['value'])) # Current wind speed, magic'd from the xml file
+    currWDir = root[4][1].attrib['code'] # Current wind direction
+    currCond = root[7].attrib['value'] # Current sky conditions
+    currTemp = round(float(root[1].attrib['value'])) # Current Tempurature
+    hold = [currTemp, currCond, currWind, currWDir]
+    return hold
 
-def drawmenu(message, weather, quote):
-    lnc = 63 #How many characters you want the screen to be wide (Line Count)
-    high = weather[0]
-    low = weather[1]
+def drawmenu(message, weather, quote, current):
+    lnc = int(eval(config['INFO']['LINE'])) #How many characters you want the screen to be wide (Line Count)
+    high = weather[1]
+    low = weather[0]
     genline = lambda mes: print(' '*((lnc - len(mes) -1)//2) + mes + ' '*((lnc - len(mes) -1)//2)) #Centre text on screen based on lnc
     weatherline = "High: " + str(high) + u'°' + "C" + "   " + "Low: " + str(low) + u'°' + "C" 
-
-    Bweather = textwrap.wrap(weatherline, lnc - 5)
+    ## Regular lines
+    Bweather = textwrap.wrap(weatherline, lnc - 5) #Ensure that lines fit line character limit
     Bmessage = textwrap.wrap(message, lnc - 5)
     Bquote = textwrap.wrap(quote, lnc - 5)
-
-   
-    #check = True if line[1] != '' else False
     
+    ## Current conditions
+    temp = 'Current Temperture: ' + str(current[0])
+    wind = str(current[2]) + ' Km/H ' + str(current[3]) + ' | ' + str(current[1])
+    
+    
+    Btemp = textwrap.wrap(temp, lnc - 5)
+    Bwind = textwrap.wrap(wind, lnc - 5)
     
     print('='*lnc) # 63 characters long
     print('')
+    for i in Btemp:
+        genline(i)
+
+    print('')
     for i in Bweather:
         genline(i)
+
     print('')
+    for i in Bwind:
+        genline(i)
+
+    print('')
+    
     for i in Bmessage:
         genline(i)
     print('')
@@ -110,5 +137,13 @@ def main():
     #print(data)
     messagex, weather = message(data)
     quote = quotes()
-    drawmenu(messagex, weather, quote)
+    current = currentx()
+    drawmenu(messagex, weather, quote, current)
+
+global config
+configx = configparser.ConfigParser()
+configx.read('config.ini')
+config = configx
+
+
 main()
